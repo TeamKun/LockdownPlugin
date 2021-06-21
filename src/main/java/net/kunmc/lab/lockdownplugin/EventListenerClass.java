3926cb358bc5a9250bd4038ce19f1cd626d07819
Yamato1989 <49328082+Yamato1989@users.noreply.github.com>
2021-06-21 16:35:01 +0900

    Add files via upload

--- src/main/java/net/kunmc/lab/lockdownplugin/EventListenerClass.java
+++ src/main/java/net/kunmc/lab/lockdownplugin/EventListenerClass.java
@@ -2,12 +2,23 @@ package net.kunmc.lab.lockdownplugin;
 
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import static org.bukkit.Bukkit.getServer;
+
 public class EventListenerClass implements Listener{
+    public boolean night() {
+        Server server = getServer();
+        String worldName = server.getWorlds().get(0).getName();
+        long time = server.getWorld(worldName).getTime() ;
+
+        return ((13000 < time) & (time < 23992));
+
+    }
     @EventHandler
     public void onHouseEvent(PlayerMoveEvent event) {
         Player p = event.getPlayer();
@@ -15,14 +26,9 @@ public class EventListenerClass implements Listener{
         int playerPositionY = loc.getBlockY();
         int highestBlock = loc.getWorld().getHighestBlockAt(loc).getY();
         GameMode g = p.getGameMode();
-        if ((playerPositionY > highestBlock)& (g.equals(GameMode.SURVIVAL))) {
+        if ((playerPositionY > highestBlock)& (g.equals(GameMode.SURVIVAL)) & night()) {
             p.setHealth(0);
         }
-    /*public void onDeath(PlayerDeathEvent event) {
-            Player p = event.getPlayer();
-            String name = p.getName();
-
-        }*/
     }
 }
 
